125c92d7d18e5998cde2cd11eff2f6e6c26d0083
Jakub <jakubgradzewicz1309@gmail.com>
2023-04-01 19:01:49 +0200

    start of the project

---
+++ src/main/java/com/github/truejacobg/remiderapp/RemiderappApplication.java
@@ -0,0 +1,13 @@
+package com.github.truejacobg.remiderapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemiderappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RemiderappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/truejacobg/remiderapp/RemiderappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.truejacobg.remiderapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RemiderappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
